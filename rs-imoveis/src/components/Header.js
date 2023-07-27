2195035fd13fa58f05dec0779023ed753d4529e3
Samuel Ricardo P. Galvão <104034355+samuelgalvao@users.noreply.github.com>
2023-07-26 21:54:11 -0300

    Day 0

---
+++ rs-imoveis/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Titulo</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
